7152be92b2c7eede73de7afb13d1046f44797c89
Sunny BIARD <sunny.biard@gmail.com>
2025-03-07 12:28:33 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+import { handleNavbarScroll, handleNavbarCollapse, createPortfolioFromJSON, createSkillsFromJSON } from "./lib.js";
+
+// Call the functions to execute the code
+handleNavbarScroll();
+handleNavbarCollapse();
+createSkillsFromJSON();
+createPortfolioFromJSON();
