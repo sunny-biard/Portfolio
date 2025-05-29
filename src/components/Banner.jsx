dde30a50f72b6c4a2e2c641bf9135459c1818336
Sunny BIARD <sunny.biard@gmail.com>
2025-05-29 22:23:56 +0200

    optimisations

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ function Banner() {
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
                         <h1 className="hero_title">Sunny BIARD</h1>
-                        <p className="hero_desc">Étudiant en Test Logiciel</p>
+                        <h2 className="hero_desc">Étudiant en Test Logiciel</h2>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+const SectionWrapper = styled.section`
+    a {
+        color: #F6F6F6;
+    }
+`
+
 function Navbar() {
     
     const [scroll, setScroll] = useState([]);
@@ -15,36 +21,38 @@ function Navbar() {
     }, [])
 
     return (
-        <nav className={`navbar-dark navbar navbar-expand-lg fixed-top navbarScroll ${scroll}`}>
-            <div className="container">
-                <a className="navbar-brand" href="#">Sunny BIARD</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#home">Accueil</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">A propos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#skills">Compétences</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#projects">Projets</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
-                        </li>
-                    </ul>
+        <SectionWrapper>
+            <nav className={`navbar-dark navbar navbar-expand-lg fixed-top navbarScroll ${scroll}`}>
+                <div className="container">
+                    <a className="navbar-brand" href="#">Sunny BIARD</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#home">Accueil</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#about">A propos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#skills">Compétences</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#projects">Projets</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
 
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </SectionWrapper>
     )
 }
 
