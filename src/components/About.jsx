9b2fd2267a7fa47ee917a46f821accce22b4bf9e
Sunny BIARD <sunny.biard@gmail.com>
2025-06-19 09:46:39 +0200

    About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,22 +3,22 @@ function About() {
 
     return (
         <section id="about" className="container">
-            <div>
-                <h2>A propos</h2>
+            <div className="containerAbout flex">
                 <div>
-                    <div className="containerAbout flex">
-                        <div>
-                            <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance dispensée par OpenClassrooms.
-                            Cette formation m'a permis d'acquérir de solides compétences aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.
-                            Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).
-                            </p>
-                        </div>
-                        <div>
-                            <h3>Formation :</h3>
-                            <p>BTS Testeur Logiciel (2024 - Aujourd'hui)</p>
-                            <p>Double Licence Économie-Gestion (2020 - 2023)</p>
-                            <p>Licence Informatique (2017 - 2020)</p>
-                        </div>
+                    <h2>A propos</h2>
+                    <div>
+                        <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance dispensée par OpenClassrooms.
+                        Cette formation m'a permis d'acquérir de solides compétences aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.
+                        Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h2>Formation :</h2>
+                    <div>
+                        <p>BTS Testeur Logiciel (2024 - Aujourd'hui)</p>
+                        <p>Double Licence Économie-Gestion (2020 - 2023)</p>
+                        <p>Licence Informatique (2017 - 2020)</p>
                     </div>
                 </div>
             </div>
