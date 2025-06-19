3f8d944844abb0e1204c39a9d4289c51925b81c5
Sunny BIARD <sunny.biard@gmail.com>
2025-06-19 14:46:55 +0200

    CSS

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner() {
 
     return (    
-        <section className="bgimage" id="home">
+        <section id="home">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
