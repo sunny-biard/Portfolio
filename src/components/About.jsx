e1fa980a6399b8b873e18ea1254ce7881f14f61d
Sunny BIARD <sunny.biard@gmail.com>
2025-06-19 22:38:06 +0200

    CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,8 @@ import styled from "styled-components"
 const AboutWrapper = styled.div`
     display: flex;
     flex-direction: row;
+    justify-content: space-evenly;
     align-items: center;
-    width: 100%;
-    h2 {
-        padding-top: 2rem;
-    }
 
     @media (max-width: 768px) {
         justify-content: center;
@@ -16,12 +13,23 @@ const AboutWrapper = styled.div`
     }
 `
 
+const AboutElement = styled.div`
+    width: 50%;
+    p {
+        padding: 1rem;
+    }
+
+    @media (max-width: 768px) {
+        width: 100%
+    }
+`
+
 function About() {
 
     return (
         <section id="about" className="container">
             <AboutWrapper>
-                <div>
+                <AboutElement>
                     <h2>A propos</h2>
                     <div>
                         <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance dispensée par OpenClassrooms.
@@ -29,15 +37,15 @@ function About() {
                         Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).
                         </p>
                     </div>
-                </div>
-                <div>
+                </AboutElement>
+                <AboutElement>
                     <h2>Formation :</h2>
                     <div>
                         <p>BTS Testeur Logiciel (2023 - Aujourd'hui)</p>
                         <p>Double Licence Économie-Gestion (2020 - 2023)</p>
                         <p>Licence Informatique (2017 - 2020)</p>
                     </div>
-                </div>
+                </AboutElement>
             </AboutWrapper>
         </section>
     )
