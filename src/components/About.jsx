6b4eadfb89b577b649951ba7b319522e99d5a4af
Sunny BIARD <sunny.biard@gmail.com>
2025-06-19 23:31:10 +0200

    CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,19 +12,7 @@ const AboutWrapper = styled.div`
     }
 `
 
-const AboutDiv = styled.div`
-    width: 50%;
-    padding: 1rem;
-    p {
-        text-align: justify;
-    }
-
-    @media (max-width: 768px) {
-        width: 100%
-    }
-`
-
-const FormationDiv = styled.div`
+const AboutElement = styled.div`
     width: 50%;
     padding: 1rem;
     li {
@@ -48,15 +36,15 @@ function About() {
     return (
         <section id="about" className="container">
             <AboutWrapper>
-                <AboutDiv>
+                <AboutElement>
                     <h2>A propos</h2>
                     <div>
                         <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance, dispensée par OpenClassrooms.</p>
                         <p>Cette formation m'a permis d'acquérir de solides compétences, aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.</p>
                         <p>Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).</p>
                     </div>
-                </AboutDiv>
-                <FormationDiv>
+                </AboutElement>
+                <AboutElement>
                     <h2>Formation</h2>
                     <ListWrapper>
                         <li>
@@ -72,7 +60,7 @@ function About() {
                             <p className="italic">(2017 - 2020)</p>
                         </li>
                     </ListWrapper>
-                </FormationDiv>
+                </AboutElement>
             </AboutWrapper>
         </section>
     )
