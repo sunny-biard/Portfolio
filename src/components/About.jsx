584f8ea4c6fdaeb9ef0d7bcb7333d04f9b52b5df
Sunny BIARD <sunny.biard@gmail.com>
2025-06-19 23:07:40 +0200

    CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,19 +32,27 @@ function About() {
                 <AboutElement>
                     <h2>A propos</h2>
                     <div>
-                        <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance dispensée par OpenClassrooms.
-                        Cette formation m'a permis d'acquérir de solides compétences aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.
-                        Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).
-                        </p>
+                        <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance dispensée par OpenClassrooms.</p>
+                        <p>Cette formation m'a permis d'acquérir de solides compétences aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.</p>
+                        <p>Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).</p>
                     </div>
                 </AboutElement>
                 <AboutElement>
-                    <h2>Formation :</h2>
-                    <div>
-                        <p>BTS Testeur Logiciel (2023 - Aujourd'hui)</p>
-                        <p>Double Licence Économie-Gestion (2020 - 2023)</p>
-                        <p>Licence Informatique (2017 - 2020)</p>
-                    </div>
+                    <h2>Formation</h2>
+                    <ul>
+                        <li>
+                            <p>BTS Testeur Logiciel</p>
+                            <p className="italic">(2023 - Aujourd'hui)</p>
+                        </li>
+                        <li>
+                            <p>Double Licence Économie-Gestion</p>
+                            <p className="italic">(2020 - 2023)</p>
+                        </li>
+                        <li>
+                            <p>Licence Informatique</p>
+                            <p className="italic">(2017 - 2020)</p>
+                        </li>
+                    </ul>
                 </AboutElement>
             </AboutWrapper>
         </section>
