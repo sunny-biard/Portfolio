e2587c127d1ad17612ec6bf1db0604f875c558d6
Sunny BIARD <sunny.biard@gmail.com>
2025-06-25 15:33:32 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,31 +14,38 @@ function Navbar() {
         });
     }, [])
 
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+
+        setOpen(!open);
+    }
+
     return (
             <nav className={`navbar-dark navbar navbar-expand-lg fixed-top navbarScroll ${scroll}`}>
                 <div className="container">
                     <a className={`navbar-brand ${scroll}`} href="#">Sunny BIARD</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="Toggle navigation">
+                        aria-label="Toggle navigation" open={open ? true : false}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item active">
-                                <a className={`nav-link ${scroll}`} href="#home">Accueil</a>
+                                <a className={`nav-link ${scroll}`} href="#home" onClick={handleClick}>Accueil</a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link ${scroll}`} href="#about">A propos</a>
+                                <a className={`nav-link ${scroll}`} href="#about" onClick={handleClick}>A propos</a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link ${scroll}`} href="#skills">Compétences</a>
+                                <a className={`nav-link ${scroll}`} href="#skills" onClick={handleClick}>Compétences</a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link ${scroll}`} href="#projects">Projets</a>
+                                <a className={`nav-link ${scroll}`} href="#projects" onClick={handleClick}>Projets</a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link ${scroll}`} href="#contact">Contact</a>
+                                <a className={`nav-link ${scroll}`} href="#contact" onClick={handleClick}>Contact</a>
                             </li>
                         </ul>
 
