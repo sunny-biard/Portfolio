42aeda7f5f8adf9eeb60d9a0be143a5ffe24abdf
Sunny BIARD <sunny.biard@gmail.com>
2025-06-25 16:21:05 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
 
     const handleClick = () => {
 
-        collapse == 'collapse' ? setCollapse('collapse.show collapsing') : setCollapse('collapse collapsing');
+        collapse == 'collapse' ? setCollapse('collapse.show') : setCollapse('collapse');
     }
 
     return (
@@ -28,7 +28,7 @@ function Navbar() {
                     <button className="navbar-toggler" type="button" aria-label="Toggle navigation" onClick={handleClick}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className={`${collapse} navbar-collapse`} id="navbarSupportedContent">
+                    <div className={`${collapse} navbar-collapse collapsing`} id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item active">
                                 <a className={`nav-link ${scroll}`} href="#home" onClick={handleClick}>Accueil</a>
