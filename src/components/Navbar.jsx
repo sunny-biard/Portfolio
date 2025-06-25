30190a56ae29e3475f0b942c1d2f7941c9994139
Sunny BIARD <sunny.biard@gmail.com>
2025-06-25 16:22:43 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
                     <button className="navbar-toggler" type="button" aria-label="Toggle navigation" onClick={handleClick}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className={`${collapse} navbar-collapse collapsing`} id="navbarSupportedContent">
+                    <div className={`${collapse} navbar-collapse`} id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item active">
                                 <a className={`nav-link ${scroll}`} href="#home" onClick={handleClick}>Accueil</a>
