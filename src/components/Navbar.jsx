352cf57ccb9d067419bb86456cb101dc42edc75f
Sunny BIARD <sunny.biard@gmail.com>
2025-06-25 15:38:26 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
             <nav className={`navbar-dark navbar navbar-expand-lg fixed-top navbarScroll ${scroll}`}>
                 <div className="container">
                     <a className={`navbar-brand ${scroll}`} href="#">Sunny BIARD</a>
-                    <button className="navbar-toggler" type="button"
+                    <button className="navbar-toggler" type="button" data-bs-toggle={open ? "collapse" : ""}
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
