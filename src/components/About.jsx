962405c0d0136ab0586306f1d62ac2dca973921f
Sunny BIARD <sunny.biard@gmail.com>
2025-06-26 23:11:33 +0200

    css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ function About() {
             <AboutWrapper className="flex">
                 <AboutElement>
                     <div>
-                        <p> Actuellement en reconversion professionnelle, je termine actuellement ma formation de BTS Testeur Logiciel en alternance, dispensée par OpenClassrooms.</p>
+                        <p> Actuellement en reconversion professionnelle, je suis une formation de BTS Testeur Logiciel en alternance, dispensée par OpenClassrooms.</p>
                         <p>Cette formation m'a permis d'acquérir de solides compétences, aussi bien en test qu'en développement mais aussi en gestion de projet, compétences que j'ai pu mettre en pratique au sein de mon entreprise.</p>
                         <p>Par ailleurs, cette dernière m'a permis de me spécialiser dans un aspect du métier qui me passionne : l'automatisation des tests. En effet, dans le cadre d'un projet conséquent, on m'a confié pour mission de développer, déployer et maintenir l'ensemble des tests automatisés d'un projet sous Selenium (Java), projet s'étalant sur 2 ans (toujours en cours).</p>
                     </div>
